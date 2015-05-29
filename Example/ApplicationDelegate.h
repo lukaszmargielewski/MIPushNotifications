b2312df05d115d451c2a9ade7ce9a30a65f0ea1e
Lukasz Margielewski <lukasz.margielewski@gmail.com>
2015-05-29 15:06:59 +0200

    Added example AppDelegate code

---
+++ Example/ApplicationDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MobileFitnessAppDelegate.h
+//  MobileFitness
+//
+//  Created by saras rani on 16/05/10.
+//  Copyright TheGreat 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ApplicationDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, strong) UIWindow *window;
+@end
+
---
+++ Example/ApplicationDelegate.m
@@ -0,0 +1,82 @@
+//
+//  MobileFitnessAppDelegate.m
+//  MobileFitness
+//
+//  Created by saras rani on 16/05/10.
+//  Copyright TheGreat 2010. All rights reserved.
+//
+
+
+
+#import "ApplicationDelegate.h"
+#import "MIPushNotifications.h"
+
+#define PUSH_NOTIFICATION_SUPPORTED 1
+
+@interface ApplicationDelegate() <MIPushNotificationsDelegate>
+
+@end
+@implementation ApplicationDelegate{
+
+
+}
+
+@synthesize timestamp_started = _timestamp_started;
+
+#pragma mark - Application delegate:
+
+
+-(BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions{
+    
+    
+#ifdef PUSH_NOTIFICATION_SUPPORTED
+    // Easiest, to make delegate self, but this can be anything:
+    [MIPushNotifications startWithLaunchOptions:launchOptions delegate:self];
+#endif
+    
+	return YES;
+}
+
+#pragma mark - MIPushNotificationsDelegate:
+
+-(void)handleRemoteNotificationWithPayload:(NSDictionary*)payload receivedWhileInForeground:(BOOL)receivedWhileInForeground{
+    
+    // Received push... again, this does not have to be in AppDelegate
+    
+}
+
+#ifdef PUSH_NOTIFICATION_SUPPORTED
+
+#pragma mark - Push iOS callbacks (redirection):
+
+-(void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken{
+    
+    [MIPushNotifications application:application didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
+    
+}
+-(void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error{
+    
+    [MIPushNotifications application:application didFailToRegisterForRemoteNotificationsWithError:error];
+}
+-(void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary*)userInfo{
+    
+    [MIPushNotifications application:application didReceiveRemoteNotification:userInfo];
+    
+}
+#endif
+
+#pragma mark - Other, standard app delegate methods:
+
+/*
+-(void)applicationWillTerminate:(UIApplication *)application{}
+
+-(void)applicationDidEnterBackground:(UIApplication *)application{}
+-(void)applicationWillEnterForeground:(UIApplication *)application{}
+
+-(void)applicationWillResignActive:(UIApplication *)application{}
+-(void)applicationDidBecomeActive:(UIApplication *)application{}
+
+-(void)applicationDidReceiveMemoryWarning:(UIApplication *)application{}
+*/
+
+@end
